8f4191bcb20a3c0616edf8d6f8efce9a7474100e
KeisukeTsuji <kei.iwmmd@gmail.com>
2021-01-13 23:07:32 +0900

    一旦仮で投票の選択肢を追加する設定画面の実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,46 @@
-import React from 'react'
+import { useState } from "react";
 
-export default () => (<div>Hello World</div>)
+const App = () => {
+  const [choices, setTodos] = useState([]);
+  const [tmpChoice, setTmpTodo] = useState("");
+
+  const addTodo = () => {
+    setTodos([tmpChoice, ...choices]);
+    setTmpTodo("");
+  };
+
+  return (
+    <>
+      <h1>quadoratic voting</h1>
+      <ul>
+        {choices.map((choice, index) => {
+          return <li key={index}>{choice}</li>;
+        })}
+      </ul>
+      <div className="form">
+        <input
+          type="text"
+          name="todo"
+          onChange={e => setTmpTodo(e.target.value)}
+          value={tmpChoice}
+        />
+        <button onClick={addTodo}>追加</button>
+      </div>
+      <style>{`
+        h1 {
+          text-align: center;
+        }
+        .form {
+          display: flex;
+          justify-content: center;
+        }
+        ul {
+          width: 200px;
+          margin: 10px auto;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default App;
