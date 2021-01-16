6ccd1574a9e7ec5b55a089060ecd47a246db33c1
KeisukeTsuji <kei.iwmmd@gmail.com>
2021-01-17 00:38:29 +0900

    仮の画面作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,42 @@
 import { useState } from "react";
 
 const App = () => {
-  const [choices, setTodos] = useState([]);
-  const [tmpChoice, setTmpTodo] = useState("");
+  const [choices, setItems] = useState([]);
+  const [tmpChoice, setTmpItem] = useState("");
 
   const addTodo = () => {
-    setTodos([tmpChoice, ...choices]);
-    setTmpTodo("");
+    setItems([tmpChoice, ...choices]);
+    setTmpItem("");
   };
 
   return (
-    <div>
-      <h1>quadoratic voting</h1>
-      <ul>
-        {choices.map((choice, index) => {
-          return <li key={index}>{choice}</li>;
-        })}
-      </ul>
+    <div className="max-w-screen-sm m-auto h-screen bg-gray-50 p-4">
       <div className="form">
         <input
           type="text"
-          name="todo"
-          onChange={e => setTmpTodo(e.target.value)}
+          name="item"
+          onChange={(e) => setTmpItem(e.target.value)}
           value={tmpChoice}
         />
-        <button onClick={addTodo}>追加</button>
+        <button onClick={addTodo}>
+          <span className="mdi mdi-plus"></span>
+        </button>
       </div>
+      <ul>
+        {choices.map((choice, index) => {
+          return (
+            <li key={index}>
+              {index}. {choice}
+            </li>
+          );
+        })}
+      </ul>
+      <div className="text-purple-500 hover:bg-purple-200 cursor-pointer">
+        save
+      </div>
+      <style>{`
+        
+      `}</style>
     </div>
   );
 };
