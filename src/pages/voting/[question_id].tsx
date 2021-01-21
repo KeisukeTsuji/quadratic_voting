02d6e3011cfd6653b7af10ddc228c452bfc3aaae
KeisukeTsuji <kei.iwmmd@gmail.com>
2021-01-22 01:22:51 +0900

    データを取得するところまで

--- src/pages/voting/[question_id].tsx
+++ src/pages/voting/[question_id].tsx
@@ -5,7 +5,25 @@ import { db } from "@/config/db";
 const App = () => {
 
   const router = useRouter();
-  console.log(router.query.question_id);
+  const questionId = router.query.question_id;
+
+  const getTarget = async () => {
+    try {
+      if (typeof (questionId) === "string") {
+        const postRef = db.collection('target').doc(questionId)
+        const postDoc = await postRef.get()
+        if (postDoc.exists) {
+          console.log(postDoc.data())
+        } else {
+          console.log('No such document!')
+        }
+      }
+    } catch (err) {
+      console.error(`Error: ${JSON.stringify(err)}`)
+    }
+  }
+
+  getTarget()
 
   return (
     <div>
