b28d6d7d33d06ccc34403d99efbcfdc1c624d8e9
KeisukeTsuji <kei.iwmmd@gmail.com>
2021-01-24 23:05:25 +0900

    prettier

--- src/pages/voting/[question_id].tsx
+++ src/pages/voting/[question_id].tsx
@@ -3,68 +3,66 @@ import { db } from "@/config/db";
 
 const QuestionId = () => {
   const [question, setQuestion] = useState("");
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
     async function getTargetAndItem() {
-      await getTarget()
-      await getItems()
+      await getTarget();
+      await getItems();
     }
-    getTargetAndItem()
-  }, [])
+    getTargetAndItem();
+  }, []);
 
   const getQuestionId = () => {
-    return 'YHzLwu1WObmuUfRW9WVK'
-  }
+    return "YHzLwu1WObmuUfRW9WVK";
+  };
 
   const getTargetRef = async () => {
-    const questionId = getQuestionId()
-    if (typeof (questionId) === "string") {
-      return await db.collection('target').doc(questionId);
+    const questionId = getQuestionId();
+    if (typeof questionId === "string") {
+      return await db.collection("target").doc(questionId);
     } else {
-      console.log("questionId is not string")
+      console.log("questionId is not string");
     }
-  }
+  };
 
   const getTarget = async () => {
     try {
-      const targetRef = await getTargetRef()
-      const targetDoc = await targetRef.get()
+      const targetRef = await getTargetRef();
+      const targetDoc = await targetRef.get();
       if (targetDoc.exists) {
-        setQuestion(targetDoc.data().question)
+        setQuestion(targetDoc.data().question);
       } else {
-        console.log('No such document!')
+        console.log("No such document!");
       }
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   const getItems = async () => {
     try {
       if (getTargetRef()) {
-        const allItems = (await getTargetRef()).collection('item')
-        const itemsGot = await allItems.get()
-        let tmpItems: Object[] = []
-        itemsGot.forEach(doc => {
+        const allItems = (await getTargetRef()).collection("item");
+        const itemsGot = await allItems.get();
+        let tmpItems: Object[] = [];
+        itemsGot.forEach((doc) => {
           if (doc.exists) {
-            tmpItems.push(doc.data())
+            tmpItems.push(doc.data());
           }
-        })
-        setItems(tmpItems)
+        });
+        setItems(tmpItems);
       }
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   return (
     <div>
       <p>{question}</p>
       {items.map((item, index) => (
-        <div key={index}>
-          {item.name}
-        </div>
+        <div key={index}>{item.name}</div>
       ))}
     </div>
   );
